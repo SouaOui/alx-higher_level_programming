8a8f77d38b39ceff7e51fea0f520663c7a7b37ee
SouaOui <soumiaouzat@gmail.com>
2023-06-06 18:58:02 -0700

    fixing the if() to bring back missing numbers

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for num in range(100):
-    if num // 10 != 9 and num % 10 != 9:
+    if num != 99:
         print("{:02d}, ".format(num), end="")
-    elif num == 99:
+    else:
         print(num)
