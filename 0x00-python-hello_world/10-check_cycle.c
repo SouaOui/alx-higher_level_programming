5493800b50af75b535aa992edfb149da0cc7ccfa
SouaOui <soumiaouzat@gmail.com>
2023-06-07 14:40:27 -0700

    is this SLL contain a cycle within it

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * check_cycle - function that check if a singly linked list has a cycle
+ * @head: pointer to a list
+ * Return: 1 if yess and 0 if NO
+ */
+#include "lists.h"
+int check_cycle(listint_t *head)
+{
+	listint_t *Tortoise = head;
+	listint_t *Hare = head->next;
+
+	if (!head || !head->next)
+		return (0);
+	while (Tortoise != Hare)
+	{
+		if (!Hare || !Hare->next)
+			return (0);
+		Tortoise = Tortoise->next;
+		Hare = Hare->next->next;
+	}
+	return (1);
+}
