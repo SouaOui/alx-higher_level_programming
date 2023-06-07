85b103dee93a6109583b595623826b0cb9eb6f7a
SouaOui <soumiaouzat@gmail.com>
2023-06-06 17:28:28 -0700

    fixing the negative number last digit adding an if statement

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = number % 10
-if last_digit > 5:
-    print(f"last digit of {number} is {last_digit} is greater than 5")
-elif last_digit == 0:
-    print(f"last digit of {number} is {last_digit} and is 0")
+if number < 0:
+    last_digit = abs(number) % 10
+    last_digit *= -1
 else:
-    print(f"last digit of {number} is {last_digit} and is less than 6 and not 0")
+    last_digit = number % 10
+if last_digit > 5:
+    print(f"Last digit of {number} is {last_digit} and is greater than 5")
+elif last_digit == 0:
+    print(f"Last digit of {number} is {last_digit} and is 0")
+else:
+    print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
