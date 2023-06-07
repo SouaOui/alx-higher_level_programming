ba81937d03dbf2c6f049392466ffc35cefc0d82c
SouaOui <soumiaouzat@gmail.com>
2023-06-06 17:42:22 -0700

    Fixing line 14 too long pycodestyle

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -11,5 +11,7 @@ if last_digit > 5:
 elif last_digit == 0:
     print(f"Last digit of {number} is {last_digit} and is 0")
 else:
-    print(f"Last digit of {number} is {last_digit} and is\
-            less than 6 and not 0")
+    print(
+        f"Last digit of {number} is {last_digit} and "
+        "is less than 6 and not 0"
+        )
