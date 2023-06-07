1037d0b54ebecf4de70ce6d777acf628105904c1
SouaOui <soumiaouzat@gmail.com>
2023-06-06 18:44:41 -0700

    without nested loops

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for num in range(100):
     if num // 10 != 9 and num % 10 != 9:
-        print("{:d}, ".format(num), end="")
+        print("{:02d}, ".format(num), end="")
     elif num == 99:
         print(num)
