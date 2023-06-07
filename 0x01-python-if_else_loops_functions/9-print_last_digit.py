6bc3cb10a415d724f854d1138b48f0cd998e6e97
SouaOui <soumiaouzat@gmail.com>
2023-06-07 15:31:52 -0700

    using abs() built in

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if (number < 0):
-        number *= -1
-        last_digit = number % 10
-        last_digit *= -1
-    else:
-        last_digit = number % 10
-    return last_digit
+        print(abs(number) % 10, end="")
+        return (abs(number) % 10)
