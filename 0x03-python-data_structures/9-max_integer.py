849984274a6297248fd5a0671138295f686c51bf
SouaOui <soumiaouzat@gmail.com>
2023-06-12 14:59:50 -0700

    Find the max without using max() function

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list == []:
+        return 'None'
+    max = my_list[0]
+    for number in my_list:
+        if number > max:
+            max = number
+    return max
