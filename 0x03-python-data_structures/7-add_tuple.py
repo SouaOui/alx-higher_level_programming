74bc9387cfbd80c34d8ed2fd48f6e96c08be132b
SouaOui <soumiaouzat@gmail.com>
2023-06-12 08:25:58 -0700

    only two elements

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     new_tuple = ()
-    for i in range(len(tuple_a)):
+    for i in range(2):
         tuple_a += (0, 0)
         tuple_b += (0, 0)
         new_tuple += (tuple_a[i] + tuple_b[i],)
