a53063dba7ecd391d18d27f93b9bc06def20a671
SouaOui <soumiaouzat@gmail.com>
2023-06-12 08:10:11 -0700

    adding two tuples

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    new_tuple = ()
+    for i in range(len(tuple_a)):
+        tuple_a += (0, 0)
+        tuple_b += (0, 0)
+        new_tuple += (tuple_a[i] + tuple_b[i],)
+    return new_tuple
