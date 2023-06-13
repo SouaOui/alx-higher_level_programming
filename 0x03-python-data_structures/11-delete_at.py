a9400b5d1e8bed8f970be2a650f0b4c654e5e92b
SouaOui <soumiaouzat@gmail.com>
2023-06-13 06:21:22 -0700

    adding the case to delete at the beginning of the list

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,6 +2,8 @@
 def delete_at(my_list=[], idx=0):
     if idx < 0 or idx > len(my_list) - 1:
         return my_list
+    if idx == 0:
+        my_list.remove(my_list[0])
     for i in my_list:
         if i == idx:
             my_list.remove(my_list[i])
