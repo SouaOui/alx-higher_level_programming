a16f3c4b329ee1d702d26feeb5bbb9e8e5b3097c
SouaOui <soumiaouzat@gmail.com>
2023-06-14 15:24:43 -0700

    not allowed to use loops

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    square_matrix = []
-    for row in matrix:
-        for col in range(len(row)):
-            square = row[col] ** 2
-            square_matrix.append(square)
-    return square_matrix
+    return list(map(lambda row: list(map(lambda x: x ** 2, row)), matrix))
