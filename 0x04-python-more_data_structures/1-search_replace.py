64fb3b434a87cbd1f9adc52f9d6b26f769aade2b
SouaOui <soumiaouzat@gmail.com>
2023-06-14 15:38:39 -0700

    1. Search and replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    my_list = my_list.copy()
+    for item in range(len(my_list)):
+        if my_list[item]==search:
+            my_list[item] = replace
+    return my_list
