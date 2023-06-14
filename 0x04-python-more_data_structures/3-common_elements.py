dfcefc164ab5a1d16a310362876c27f400dec293
SouaOui <soumiaouzat@gmail.com>
2023-06-14 16:12:01 -0700

    Present in Both sets

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -3,6 +3,6 @@ def common_elements(set_1, set_2):
     intersect = set()
     for x in set_1:
         for y in set_2:
-            if x==y:
+            if x == y:
                 intersect.add(x)
     return intersect
