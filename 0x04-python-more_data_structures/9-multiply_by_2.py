240f9dc4b37c1c363a2fdff480d2f340d77a27fb
SouaOui <soumiaouzat@gmail.com>
2023-06-14 17:43:13 -0700

    pycodestyle is happy

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    return dict(list(map(lambda x, y: (x, y * 2),a_dictionary.keys(), a_dictionary.values())))
+    return dict(list(map(lambda x, y: (x, y * 2),
+                         a_dictionary.keys(), a_dictionary.values())))
