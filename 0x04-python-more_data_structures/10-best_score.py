e13820374e4f8b52bcef01b50029b02c546a3481
SouaOui <soumiaouzat@gmail.com>
2023-06-14 18:14:24 -0700

    print the name of the best student

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,9 @@ def best_score(a_dictionary):
     if not a_dictionary:
         return None
     bestscore = 0
-    for current_score in a_dictionary.values():
+    bestkey = None
+    for current_student, current_score in a_dictionary.items():
         if current_score > bestscore:
+            bestkey = current_student
             bestscore = current_score
-    return bestscore
+    return bestkey
