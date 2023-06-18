566de83adb25ceba0d73675960f10f0b2e5f4149
SouaOui <soumiaouzat@gmail.com>
2023-06-18 20:40:57 +0100

    15. Remove at position

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,8 @@
+def remove_char_at(str, n):
+    if str == "" or n > len(str) - 1:
+        return str
+    str_cpy = ""
+    for i in range(len(str)):
+        if i != n:
+            str_cpy += str[i]
+    return str_cpy
