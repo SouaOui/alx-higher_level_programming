868444cf5db804e992e0ed6a0b1965e5c146679e
SouaOui <soumiaouzat@gmail.com>
2023-06-18 02:18:33 +0100

    13. Weighted average! Access a tuple in a list

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,7 @@
+def weight_average(my_list=[]):
+    average = 0
+    if my_list == []:
+        return 0
+    for item in my_list:
+        average += (item[1] * item[0]) / sum([x[1] for x in my_list])
+    return average
