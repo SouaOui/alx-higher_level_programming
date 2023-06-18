eaed0fa3ce546250aec71053e5ed95f803034ae3
SouaOui <soumiaouzat@gmail.com>
2023-06-18 18:16:31 +0100

    test the code

---
+++ 0x01-python-if_else_loops_functions/13-main.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code for
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    listint_t *head;
+
+    head = NULL;
+    add_nodeint_end(&head, 10);
+    add_nodeint_end(&head, 12);
+    print_listint(head);
+
+    printf("-----------------\n");
+
+    insert_node(&head, 27);
+
+    print_listint(head);
+
+    free_listint(head);
+
+    return (0);
+}
