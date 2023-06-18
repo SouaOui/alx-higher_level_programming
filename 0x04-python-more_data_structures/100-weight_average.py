3acb5fb3878c0f29ba8c91250049e9ecb852dd7a
SouaOui <soumiaouzat@gmail.com>
2023-06-18 02:25:42 +0100

    Deleted

--- 0x04-python-more_data_structures/100-weight_average.py
+++
@@ -1,7 +0,0 @@
-def weight_average(my_list=[]):
-    average = 0
-    if my_list == []:
-        return 0
-    for item in my_list:
-        average += (item[1] * item[0]) / sum([x[1] for x in my_list])
-    return average
