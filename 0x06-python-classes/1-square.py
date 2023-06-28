114877dd7fe0490a500d56e3aa2de5b5db74ba66
SouaOui <soumiaouzat@gmail.com>
2023-06-28 01:17:25 +0100

    create a private instance attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""define a class that calculate a square of number"""
+
+
+class Square:
+    """Represent a operation square"""
+    def __init__(self, size):
+        self.__size = size
