0c7acc9a967009c69143667c473e859e2a0a557d
SouaOui <soumiaouzat@gmail.com>
2023-06-28 23:46:53 +0100

    print to the stderr and remove the function to check if value is integer

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+import sys
+
 def safe_print_integer_err(value):
     try:
-        if isinstance(value, int):
-            print("{:d}".format(value))
-            return True
-    except TypeError:
+        print("{:d}".format(value))
+        return True
+    except (TypeError, ValueError) as say:
+        sys.stderr.write("Exception: {}\n".format(say))        
         return False
