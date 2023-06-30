6c88184329ed3fe1b27c750c6d91ad57073ea4de
SouaOui <soumiaouzat@gmail.com>
2023-06-30 10:05:07 +0100

    make a script Dynamic

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 from sys import argv
 
-len = len(argv)-1
-if len == 0:
-    print("0: Arguments.")
-elif len == 1:
-    print("{} argument:\n{}: {}".format(len, len, argv[len]))
-else:
-    print("{} arguments:".format(len))
-    for i in range(len):
-        print("{}: {}".format(i+1, argv[i + 1]))
+if __name__ == "__main__":
+    len = len(argv)-1
+    if len == 0:
+        print("0: Arguments.")
+    elif len == 1:
+        print("{} argument:\n{}: {}".format(len, len, argv[len]))
+    else:
+        print("{} arguments:".format(len))
+        for i in range(len):
+            print("{}: {}".format(i+1, argv[i + 1]))
