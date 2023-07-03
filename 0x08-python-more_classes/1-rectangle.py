3d47715b46afab7b73ee6b841d90f6aac3d28c8c
SouaOui <soumiaouzat@gmail.com>
2023-07-03 18:43:15 +0100

    adding the docstring

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,16 +1,30 @@
-#!/usr/bin/python3
-"""Declare a class Rectangle"""
+/bin/bash: wq: command not found
+"""
+Declaring a class Rectangle and defining methods
+"""
 
 
 class Rectangle:
     """define a class of Rectangle"""
     def __init__(self, width=0, height=0):
-        self.__width = width
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
         self.__height = height
+        self.__width = width
 
     @property
     def height(self):
         return self.__height
+
+    @property
+    def width(self):
+        return self.__width
 
     @height.setter
     def height(self, value):
@@ -20,10 +34,6 @@ class Rectangle:
             raise ValueError("height must be >= 0")
         else:
             self.__height = value
-
-    @property
-    def width(self):
-        return self.__width
 
     @width.setter
     def width(self, value):
