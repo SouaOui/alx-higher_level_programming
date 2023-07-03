4dc7c79e93d223de2335453d25b51b4e16b03019
SouaOui <soumiaouzat@gmail.com>
2023-07-03 18:55:20 +0100

    fixing pycodestyle

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -49,5 +49,5 @@ class Rectangle:
 
     def perimeter(self):
         if self.__width == 0 or self.__height == 0:
-            return 0 
+            return 0
         return (self.__width + self.__height) * 2
