d638e409d68eea0cdf81a4f3f278c9a4f0616873
SouaOui <soumiaouzat@gmail.com>
2023-07-03 18:16:09 +0100

    checking in the init method

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -2,13 +2,16 @@
 class Rectangle:
     """define a class of Rectangle"""
     def __init__(self, width=0, height=0):
-        if not isinstance(width, int) or not isinstance(height, int):
-            raise TypeError
-        elif width < 0 or height < 0:
-            raise ValueError
-        else:
-            self.__width = width
-            self.__height = height
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+        self.__height = height
 
     @property
     def height(self):
