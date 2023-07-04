6ac0c58649306b9cbb94b986fbd41537a4267c03
SouaOui <soumiaouzat@gmail.com>
2023-07-04 21:28:27 +0100

    dividing each element of the matrix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -6,6 +6,7 @@ def matrix_divided(matrix, div):
     """
     Matrix Divide
     """
+    new_matrix = []
     for item in matrix:
         if not isinstance(item, list):
             raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
@@ -13,8 +14,19 @@ def matrix_divided(matrix, div):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
+    try:
+        size = len(matrix[0])
+    except Exception:
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
     for item in matrix:
+        if (not isinstance(item, list)):
+            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        if len(item) != size:
+            raise TypeError("Each row of the matrix must have the same size")
+        new_row = []
         for num in item:
             num /= div
             num = round(num, 2)
-    return (matrix)
+            new_row.append(num)
+        new_matrix.append(new_row)
+    return (new_matrix)
