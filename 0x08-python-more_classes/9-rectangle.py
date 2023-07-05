c991da8bca84e1426fd3864cf0c2fc68fcfedc79
SouaOui <soumiaouzat@gmail.com>
2023-07-05 16:26:28 +0100

    adding docstring

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -68,10 +68,6 @@ class Rectangle:
             return 0
         return (self.__width + self.__height) * 2
 
-    @classmethod
-    def square(cls, size=0):
-        return (cls(int(size), int(size)))
-
     def __str__(self):
         if self.__width == 0 or self.__height == 0:
             return ""
@@ -92,6 +88,7 @@ class Rectangle:
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
+        """returns the biggest area rectangle"""
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
@@ -101,4 +98,8 @@ class Rectangle:
         if rect_1.area() < rect_2.area():
             return rect_2
         return rect_1
-    
+
+    @classmethod
+    def square(cls, size=0):
+        """return rectangle and hieght equal to size"""
+        return (cls(size, size))
