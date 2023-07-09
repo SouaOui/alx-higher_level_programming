2207dfda08b1d9a0300f390c228fe235b81769b9
SouaOui <soumiaouzat@gmail.com>
2023-07-09 23:03:01 +0100

    testing file

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,15 +3,13 @@
 Addition Module
 """
 
-
 def add_integer(a, b=98):
-    """Addition of two integers"""
-    if (not isinstance(a, int) and not isinstance(a, float)):
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if (not isinstance(b, int) and not isinstance(b, float)):
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
     if isinstance(a, float):
         a = int(a)
     if isinstance(b, float):
         b = int(b)
-    return a + b
+    return a + b 
