34b6c800452039ca8e3a281802db066db9dccb59
SouaOui <soumiaouzat@gmail.com>
2023-07-10 23:31:21 +0100

    py+changes

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -6,11 +6,15 @@ class Square(Rectangle):
     """Represent a rectangle using BaseGeometry."""
 
     def __init__(self, size):
-        """Intialize a new Rectangle.
+        """Intialize a new square.
 
         Args:
-            size (int): The height of the new Rectangle.
+            size (int): The size of the new Rectangle.
         """
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
+
+    def area(self):
+        """implement area method for square"""
+        return self.__size ** 2
