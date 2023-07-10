4144a05fd5e7023aeb4363e15e073c6d91673581
SouaOui <soumiaouzat@gmail.com>
2023-07-10 23:18:17 +0100

    inherit from inherit class

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Represent a rectangle using BaseGeometry."""
+
+    def __init__(self, size):
+        """Intialize a new Rectangle.
+
+        Args:
+            width (int): The width of the new Rectangle.
+            height (int): The height of the new Rectangle.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
