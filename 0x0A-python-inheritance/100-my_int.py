1fbab35516c988ce30e341a1c83c81375a24fb85
SouaOui <soumiaouzat@gmail.com>
2023-07-11 00:27:52 +0100

    rebel effect

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -5,8 +5,8 @@
 class MyInt(int):
     def __eq__(self, other):
         """is equal"""
-        return super().__ne__(other)
+        return self.real != other
 
     def __ne__(self, other):
         """not equal"""
-        return super().__eq__(other)
+        return self.real == other
