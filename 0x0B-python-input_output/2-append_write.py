fa1f3b0c3ee5c9f8f44aa79f0b8c2b7a9d97dbc4
SouaOui <soumiaouzat@gmail.com>
2023-07-11 18:08:25 +0100

    append to a file

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""
+Define append function to a file
+"""
+
+
 def append_write(filename="", text=""):
     """append to file"""
     with open(filename, 'a', encoding="utf-8") as file:
         return file.write(text)
-        file.tell()
