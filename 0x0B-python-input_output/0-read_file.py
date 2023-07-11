66c9a4d8650be03d87ec6ccbc1abce4d25ce3bb9
SouaOui <soumiaouzat@gmail.com>
2023-07-11 17:04:11 +0100

    readme file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,4 +7,5 @@ read the file
 def read_file(filename=""):
     """read the file"""
     with open("filename", encoding="utf-8") as file:
-        print(file.read(), end="")
+        read_data = file.read()
+        print(read_data, end="")
