bc1dacf21712af28af08750119252fc5015802d0
SouaOui <soumiaouzat@gmail.com>
2023-07-13 01:54:22 +0100

    appending to a text a new string after a line

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
+"""Defines a text file insertion function."""
+
 def append_after(filename="", search_string="", new_string=""):
     """
     inserts a line of text to a file
     after a line containing that specifc string
     """
-    with open(filename, 'r') as file:
-        lines = file.readlines()
-    with open(filename, 'w') as file:
-        for line in lines:
-            file.write(line)
+    text = ""
+    with open(filename, "r", encoding='utf-8') as file1:
+        for line in file1:
+            text += line
             if search_string in line:
-                file.write(new_string)
+                text += new_string
+    with open(filename, "w", encoding="utf-8") as file2:
+        file2.write(text)
---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def pascal_triangle(n):
+    """
+    returns list of lists of integers 
+    representing in Pascal's Triangle
+    """
+    if n <= 0:
+        returns [[]]
+    for i 
+    
