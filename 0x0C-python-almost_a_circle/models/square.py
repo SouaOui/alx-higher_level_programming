7400395a92339cc373b87d43fc192b87c5c40f19
SouaOui <soumiaouzat@gmail.com>
2023-07-17 22:11:29 +0100

    inherit from rectangle

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Create a SubClass from Rectangle Class (SubClass of Base)"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+    Write the class Rectangle that inherits
+    from Rectanlge(SubClass of Base)
+    """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """initialize attribute for square class"""
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """getter method for width"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """setter method for width"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value <= 0:
+            raise ValueError("size must be > 0")
+        self.__width = value
+
+    def __str__(self):
+        """print the information of a class in good way"""
+        new_string = ""
+        new_string += "[square] "
+        new_string += "({}) {}/{} ".format(self.id, self.x, self.y)
+        new_string += "- {}".format(self.size)
+        return new_string
