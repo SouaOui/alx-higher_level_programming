31f671d8564362db27ffbcc486bea959f3b4530c
SouaOui <soumiaouzat@gmail.com>
2023-07-18 23:48:31 +0100

    update

---
+++ 0x0C-python-almost_a_circle/00-main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Check """
+from models.square import Square
+
+# s = Square(2)
+# if str(s) != "[Square] (1) 0/0 - 2":
+#     print("__str__ is not correctly overloaded: {}".format(s))
+#     exit(1)
+
+s = Square(2, 3)
+if str(s) != "[Square] (2) 3/0 - 2":
+    print("__str__ is not correctly overloaded: {}".format(s))
+    exit(1)
+
+s = Square(2, 3, 4)
+if str(s) != "[Square] (3) 3/4 - 2":
+    print("__str__ is not correctly overloaded: {}".format(s))
+    exit(1)
+
+s = Square(2, 3, 4, 89)
+if str(s) != "[Square] (89) 3/4 - 2":
+    print("__str__ is not correctly overloaded: {}".format(s))
+    exit(1)
+
+print("OK", end="")
