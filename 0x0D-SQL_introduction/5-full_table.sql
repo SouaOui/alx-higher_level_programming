c5fe5e76fc166a8f7d58ac3d9e91dc48f2983a4c
SouaOui <soumiaouzat@gmail.com>
2023-08-15 14:48:41 -0700

    change it to create to retrieve orginal

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,2 +1,4 @@
 -- prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.
-SHOW COLUMNS FROM first_table;
+-- SHOW COLUMNS FROM first_table;
+-- a way to see the exact structure of the table as it was originally created.
+SHOW CREATE TABLE first_table;
