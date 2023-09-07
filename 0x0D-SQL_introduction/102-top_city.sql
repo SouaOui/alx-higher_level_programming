f422815892c54753e7ba5d1b4dfd0531b42d8fd4
SouaOui <soumiaouzat@gmail.com>
2023-09-07 19:47:35 +0100

    top 3 cities average temperature July and August

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,3 +1,3 @@
 -- displays the average temperature (Fahrenheit) by city ordered by temperature (descending)
 -- ONLY top 3 LIMIT top three 
-SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
+SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month in (7, 8) GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
