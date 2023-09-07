666d0fffd8e932d73ea9c98b7f39c15dcb42fa92
SouaOui <soumiaouzat@gmail.com>
2023-09-07 17:00:10 +0100

    Convert UTF-8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,5 @@
 -- Convert the DATABASE to utf-8 and also the table and field name
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE hbtn_0c_0;
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-UPDATE first_table SET name = CONVERT(name USING utf8mb4);
+ALTER TABLE first_table MODIFY name VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci;
