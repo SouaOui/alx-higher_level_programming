7789cd68e2c7fb36ddcb71f67c4b6608e828822b
SouaOui <soumiaouzat@gmail.com>
2023-09-08 18:28:49 +0100

    grant select permission on user on database

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
--- create a user and grant them all permisions on your sql server
+-- Creates the database hbtn_0d_2 and the user user_0d_2
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-CREATE DATABASE hbtn_0d_2 IF NOT EXISTS;
-GRANT SELECT ON hbtn_0d_2 TO 'user_0d_2'@'localhost';
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
