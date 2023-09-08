044150426b7c76b8096f1bfe9ac46d307655bcfe
SouaOui <soumiaouzat@gmail.com>
2023-09-08 19:26:16 +0100

    Foreign key

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,8 @@
+-- create a database hbtn_0d_usa and create the table 'cities'
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (
+	id INT NOT NULL PRIMARY KEY UNIQUE AUTO_INCREMENT,
+	name VARCHAR(256) NOT NULL,
+	FOREIGN KEY(state_id) REFERENCES states(id));
+ENGINE=InnoDB;
