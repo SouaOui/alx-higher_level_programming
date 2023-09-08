4a57b7837b0d56c993dbf75086016647f87972d5
SouaOui <soumiaouzat@gmail.com>
2023-09-08 19:09:53 +0100

    Create newdatabase and table

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,5 @@
+-- create a database hbtn_0d_usa and create the first table in it 'states'
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states(id INT UNIQUE NOT NULL PRIMARY KEY AUTO INCREMENT,
+name VARCHAR(256) NOT NULL);
