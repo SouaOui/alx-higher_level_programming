5a9dde44405ac03c20fa39b7d436457cb1966b8c
SouaOui <soumiaouzat@gmail.com>
2023-09-12 00:25:14 +0100

    Javascript is amazing

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const args = process.argv;
+const n = parseInt(args[2]);
+function factorial (n) {
+  if (n === 0 || n === 1 || n === 'NaN') {
+    return (1);
+  }
+  return n * factorial(n - 1);
+}
+console.log(factorial(n));
