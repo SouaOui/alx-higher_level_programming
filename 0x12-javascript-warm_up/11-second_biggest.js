3d984df7198a5e1334b46b2c6911def74b208166
SouaOui <soumiaouzat@gmail.com>
2023-09-12 00:39:15 +0100

    Javascript is amazing

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,14 @@
 #!/usr/bin/node
 const args = process.argv;
 const length = args.length;
-let max = parseInt(args[2]);
-for (let i = 0; i < length; i++) {
-  if (parseInt(args[i]) > max) {
-    max = parseInt(args[i]);
+if (length === 2 || length === 1) {
+  console.log(0);
+} else {
+  let max = parseInt(args[2]);
+  for (let i = 0; i < length; i++) {
+    if (parseInt(args[i]) > max) {
+      max = parseInt(args[i]);
+    }
   }
+  console.log(max);
 }
-console.log(max);
