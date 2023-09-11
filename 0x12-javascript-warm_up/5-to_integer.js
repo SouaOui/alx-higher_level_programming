abe98c9727aa5abd34df4417d394a45efd87e1df
SouaOui <soumiaouzat@gmail.com>
2023-09-11 21:58:42 +0100

    Javascript is amazing

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const process = require('process');
 const args = process.argv;
-const message = 'My Number: '
+const message = 'My Number: ';
 const numberInt = parseInt(args[2], 10);
 if (!isNaN(numberInt)) {
   console.log(message + numberInt);
