1d0ce5425de34a856017c0b1785eebbff7e10d17
SouaOui <soumiaouzat@gmail.com>
2023-09-14 00:48:34 +0100

    javascript class

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const SquareBase = require('./5-square');
+
+class Square extends SquareBase {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c = 'X') {
+    for (let i = 0; i < this.width; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+module.exports = Square;
