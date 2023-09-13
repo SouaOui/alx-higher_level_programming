7aed2a57767f263ee880eaea8860b50299cedee4
SouaOui <soumiaouzat@gmail.com>
2023-09-13 22:40:34 +0100

    Javascript objects

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) { return 'Rectangle{}'; }
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) { console.log('X'.repeat(this.width)); }
+  }
+}
+module.exports = Rectangle;
