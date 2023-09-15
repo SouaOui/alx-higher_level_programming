2c88af5ab671948ef53efa04f29122e54c4d4d6c
SouaOui <soumiaouzat@gmail.com>
2023-09-15 04:07:14 +0100

    When imported don't execute

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """getting started with the python module MySQLdb"""
 
-import MySQLdb
+if __name__ == "__main__":
+	import MySQLdb
 
-conn = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="root", db="hbtn_0e_0_usa", charset="utf8")
-cur = conn.cursor()
-cur.execute("SELECT * FROM states ORDER BY id ASC") # HERE I have to know SQL to grab all states in my database
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-conn.close()
+	conn = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="root", db="hbtn_0e_0_usa", charset="utf8")
+	cur = conn.cursor()
+	cur.execute("SELECT * FROM states ORDER BY id ASC") # HERE I have to know SQL to grab all states in my database
+	query_rows = cur.fetchall()
+	for row in query_rows:
+		print(row)
+	cur.close()
+	conn.close()
 
