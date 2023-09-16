ca5eb91b6f9df41dbbb055c5af92977af3470303
SouaOui <soumiaouzat@gmail.com>
2023-09-16 22:21:23 +0100

    Third Task

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     cur = conn.cursor()
     # HERE I have to know SQL to grab all states in my database
     cur.execute("SELECT * FROM states WHERE name LIKE BINARY %(state_name)s \
-        ORDER BY id ASC", sys.argv[4])
+        ORDER BY id ASC", {'state_name': sys.argv[4]})
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
