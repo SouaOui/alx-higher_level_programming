52b6827e3a5cd41e9689158dd7e68c58fffbff0e
SouaOui <soumiaouzat@gmail.com>
2023-09-17 13:32:32 +0100

    Add to states table and print the id

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Fetch All the Data From the table states"""
+import sys
+from sqlalchemy import create_engine, select
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    sentence = 'mysql+mysqldb://{}:{}@localhost/{}'
+    engine = create_engine(sentence.format(
+        username, password, database), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    # state = session.query(State.id).where(State.name == name_state).first()
+    new_state = State(name='Louisiana')
+    session.add(new_state)
+    # print(state) -> None so the add didn't return anything
+    # state = session.query(State.id).where(State.name == 'Louisiana').first()
+    state = session.query(State.id).where(State.name == new_state.name).first()
+    if state:
+        print(state.id)
+    else:
+        print('Not found')
