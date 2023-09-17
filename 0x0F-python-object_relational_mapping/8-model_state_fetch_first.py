0e7311b08f9fb9f53f1030e8bf6974bf0d861c96
SouaOui <soumiaouzat@gmail.com>
2023-09-17 12:48:18 +0100

    Get the first row if exist

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -17,7 +17,8 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
     states = session.query(State).order_by(State.id.asc()).limit(1)
-
     for state in states:
-        # print(state)
-        print('{}: {}'.format(state.id, state.name))
+        if state:
+            print('{}: {}'.format(state.id, state.name))
+        else:
+            print('Nothing')
