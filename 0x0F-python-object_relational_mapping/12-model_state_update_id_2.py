899a341931ca5cd1e49e74002fc0822a9fcaa70c
SouaOui <soumiaouzat@gmail.com>
2023-09-17 14:01:30 +0100

    Update the name of State based on id

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -21,6 +21,6 @@ if __name__ == "__main__":
     session = Session()
     # state = session.query(State).where(State.id ==2).
     # update({state.name:'New Mexico'})
-    session.query(State).filter(State.id == 1).update(
+    session.query(State).filter(State.id == 2).update(
         {State.name: 'New Mexico'})
     session.commit()
