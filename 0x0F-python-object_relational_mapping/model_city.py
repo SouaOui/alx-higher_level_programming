524fc4adc582c1b67d6fbfa86b414045f52ac7f0
SouaOui <soumiaouzat@gmail.com>
2023-09-18 00:00:37 +0100

    City to map to table cities

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Declaration of the Class State"""
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from model_state import State, Base
+Base = declarative_base()
+
+
+class City(Base):
+    """Create a City class to represent the cities table in db
+
+    Attributes:
+        City  (id): id of the state
+        City  (name): name of the state
+        City  (state_id): id of the state which city belong to
+    """
+    __tablename__ = 'cities'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'))
+    state = relationship('State', backref="cities")
