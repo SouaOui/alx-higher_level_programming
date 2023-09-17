e544d7d960d2f92e7dce284e83693aa17800617b
SouaOui <soumiaouzat@gmail.com>
2023-09-17 13:24:58 +0100

    get the id of search state

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -4,13 +4,13 @@ import sys
 from sqlalchemy import create_engine, select
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
-username = sys.argv[1]
-password = sys.argv[2]
-database = sys.argv[3]
-name_state = sys.argv[4]
 
 
 if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    name_state = sys.argv[4]
     sentence = 'mysql+mysqldb://{}:{}@localhost/{}'
     engine = create_engine(sentence.format(
         username, password, database), pool_pre_ping=True)
