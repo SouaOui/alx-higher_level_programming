2aed50b74b092efb29d611b339e9d65c4b9ebd3f
SouaOui <soumiaouzat@gmail.com>
2023-09-29 22:17:35 +0100

    "6. Find a peaké!

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def find_peak(list_of_integers):
+    """find_peak
+
+    Args:
+        list_of_integers (list): list of integers
+
+    Returns:
+        integer: the peak of the given list
+    """
+    if len(list_of_integers) == 0:
+        return None
+    peak = list_of_integers[0]
+    for ele in list_of_integers:
+        if ele > peak:
+            peak = ele
+    return peak
