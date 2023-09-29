46afc22ef931367e5bc356a64ac99b3f2174dbd1
SouaOui <soumiaouzat@gmail.com>
2023-09-29 23:43:02 +0100

    giving permissions

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,11 +8,11 @@ https://alx-intranet.hbtn.io/status
 import urllib.request
 with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as f:
     data = f.read()
-    result_decode = data.decode('utf-8')
+    result_decode = data.decode('UTF-8')
     print("Body response:")
-    print("    - type: {}".format(type(data)))
-    print("    - content: {}".format(data))
-    print("    - utf8 content: {}".format(result_decode))
+    print("\t- type: {}".format(type(data)))
+    print("\t- content: {}".format(data))
+    print("\t- utf8 content: {}".format(result_decode))
 
 # index = data.find('utf8 content:')
 # print(index)
