8055a1bd963da26558172147026ec34edc1b143a
SouaOui <soumiaouzat@gmail.com>
2023-09-29 21:57:44 +0100

    5. cURL POST parameters!

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes URL passed display the body of the response
-curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
+curl -s -X POST -d '{"email":"test@gmail.com", "subject":"I will always be here for PLD"}' "$1"
