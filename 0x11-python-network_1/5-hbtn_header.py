45ce176885e454fd2ef02c82389fa681ec36e647
SouaOui <soumiaouzat@gmail.com>
2023-10-03 12:53:10 +0100

    5. Response header value #1

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""  """
+import requests
+import sys
+
+if __name__ == "__main__":
+    Url = sys.argv[1]
+    response = requests.get(Url)
+    print(response.headers.get("X-Request-Id"))
