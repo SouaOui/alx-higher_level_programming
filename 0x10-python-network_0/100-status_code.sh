dfa794c54e46a72f5c0697520fae26bd518d5e54
SouaOui <soumiaouzat@gmail.com>
2023-10-03 12:32:56 +0100

    7. Only status code

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a Bash script that sends a request to a URL passed as an argument, and displays only the status code of the response.
-curl -s -o /dev/null -w "%{http_code}" "$1"
+curl -sw '%{http_code}' -o /dev/null "$1"
