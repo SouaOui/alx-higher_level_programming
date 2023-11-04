34ca6f48f9aea0a6385b38b4083b6eed4ec27a16
SouaOui <soumiaouzat@gmail.com>
2023-11-04 10:14:49 +0100

    js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
+
 const fs = require('fs');
 
 if (process.argv.length !== 4) {
-  console.log('Usage: node write_file.js <file_path> <data-write>');
+  console.log('Usage: node write_file.js <file_path> <data-to-write>');
   process.exit(1);
 }
 
@@ -11,8 +12,6 @@ const dataToWrite = process.argv[3];
 
 fs.writeFile(filePath, dataToWrite, 'utf-8', (err) => {
   if (err) {
-    console.error('An error occurred:', err);
-  } else {
-    console.log('Data has been written to the file successfully.');
+    console.error(err);
   }
 });
